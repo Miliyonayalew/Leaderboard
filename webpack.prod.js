6438e3df33b771ce8e640e11c6325e502926bddd
Miliyonayalew <miliayalew@gmail.com>
2022-10-12 00:43:13 +0300

    Added requested changes

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
