d301f23b69699ad7fc25a2bdd5689f3fd9b040d4
Miliyonayalew <miliayalew@gmail.com>
2022-10-12 20:15:36 +0300

    Implement referesh button and orginize the code in module

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './style.css';
+import fetcher from './modules/fetcher.js';
+import send from './modules/sender.js';
+
+send();
+document.querySelector('.refresh-btn').addEventListener('click', () => {
+  fetcher();
+});
+
+window.addEventListener('load', () => {
+  fetcher();
+});
