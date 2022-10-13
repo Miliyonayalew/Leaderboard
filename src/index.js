ef9223db4e1c4be60bf6521ff838300413187322
Miliyonayalew <miliayalew@gmail.com>
2022-10-13 20:21:24 +0300

    build the project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css';
 import fetcher from './modules/fetcher.js';
 import send from './modules/sender.js';
-import close from './modules/notify.js';
+import { close } from './modules/notify.js';
 
 send();
 close();
